ae3d4202eb54a5ae24fa4e23c26532da0c5dd105
unknown <temakot254@gmail.com>
2023-04-10 09:07:27 +0300

    cars`

--- js/main.js
+++ js/main.js
@@ -26,4 +26,37 @@ function checkTime(i){
   if(i < 10) {i = "0" + i}
   return i
 }
+const clear = document.querySelector(`.icon`)
+let head = document.querySelector(`.h1`)
+
+window.onload = () => {
+  let input = document.querySelector(`input`.toUpperCase())
+  input.oninput = function(){
+      let value = this.value.trim()
+
+      let list = document.querySelectorAll(`.main .patt`)
+
+      if(value !== "") {
+        clear.onclick = function(){
+          document.getElementById(`mySearch`).value = ``
+        }
+         list.forEach(elem => {
+              if(elem.innerText.search(value) == -1){
+                  elem.classList.add(`hide`)
+              }
+          })
+      }else{
+        clear.onclick = function(){
+          document.getElementById(`mySearch`).value = ``
+        }
+          list.forEach(elem =>{
+              elem.classList.remove(`hide`)
+          })
+      }
+  }
+}
+
+clear.onclick = function(){
+  document.getElementById(`mySearch`).value = ``
+}
 
